c6c20e333730b87b6205cfb9085ed8d1ee911de3
Aparna Karve <akarve@redhat.com>
2018-12-12 17:48:54 -0800

    set no UI

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,8 @@
 import connexion
 
 # Create the application instance
-application = connexion.App(__name__, specification_dir="./")   #noqa
+options = {"swagger_ui": False}
+application = connexion.App(__name__, specification_dir="./", options=options)   #noqa
 
 # read the swagger.yml file to configure the endpoints
 application.add_api("swagger.yml")
